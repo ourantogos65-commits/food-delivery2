3e4d1ed9372bf958212d51ef63d2e9ccd0df907c
25LP5593 25LP5593 <25LP5593@G303-CC0156.local>
2025-11-04 15:40:04 +0800

    sign-up

---
+++ backend/src/routers/router.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {  signUp } from "../controlled/user.js";
+
+
+export const Routers = Router();
+
+Routers
+  .post("/signup", signUp)
+
+
+
+//   .post("/create-user", CreateUser)
+//   .get("/user", getUserByid)
+//   .get("/getFoodOrder", getfoodOrder);
+
+// .get("/user/:id", verifyToken, getUserByid);
